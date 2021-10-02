540c8ecd5f53d77ac06cc1df4a7eb668c121e888
Gaurav Singh <72642688+sgaurav37533@users.noreply.github.com>
2021-10-02 23:34:49 +0530

    Create 868-A.cpp

---
+++ CodeForces Problems/868-A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std ;
+ 
+int main() {
+    string s1 , s2 ;
+    int n , a = 0 , b = 0 ;
+    cin >> s1 >> n ;
+    for(int i = 0 ; i < n ; i++){
+        cin >> s2 ;
+        if(s1 == s2){
+            cout << "YES\n" ;
+            return 0;
+        }
+        if(s1[0] == s2[1])
+            a++;
+        if(s1[1] == s2[0])
+            b ++ ;
+    }
+    if(a >= 1 && b >= 1)
+        cout << "YES\n" ;
+    else
+        cout << "NO\n" ;
+}
