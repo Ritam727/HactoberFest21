459fd8ae19705eef1d7b398e93c5c691d3c3a896
Anand <73114463+Anand4756@users.noreply.github.com>
2021-10-03 14:34:45 +0530

    1D array solution hackerrank

---
+++ Java HackerRank/Data Structures/Java 1D Array/Array.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Array {
+
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
