52814eefd37000bcb93752cb915f39468d16b05d
Rohan Saini <83878702+rohansaini886@users.noreply.github.com>
2021-10-03 16:25:59 +0530

    Changed the program using recursive function

--- Python/display_sum_of_first_even_n_numbers.py
+++ Python/display_sum_of_first_even_n_numbers.py
@@ -1,10 +1,11 @@
-#pytho program to display sum of first 'n' even numbers
-n=(int)(input("Enter an integer number"))
-sum=0
-num=2
-for i in range(1,n+1):
-    sum=sum+num
-    num=num+2
+sum = 0 # IInitializing the varraible sum
+def sum_of_n_terms(n): # Initializing the function
+    global sum
+    if n == 0:
+        return sum
+    else:
+        return n + sum_of_n_terms(n-1)
 
-print("The sum of first",n,"even number is",sum)
-    
+n = int(input("Enter the Number :- ")) # Taking the input or the number till which you have to find the sum
+ans = sum_of_n_terms(n) # Called the function
+print(ans) # Printing the result.
