d9e538b7205f14d178406f103a92e15ecf3d6b9d
Prasant <77951529+prasant2603@users.noreply.github.com>
2021-10-05 08:46:45 +0530

    Create divideUsingBitManipulation.cpp
    Bit Manipulation

---
+++ C++/divideUsingBitManipulation.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int divide(long long dev, long long div) {
+  int sign = ((dev < 0) ^
+              (div < 0)) ? -1 : 1;
+  // remove sign of operands
+  dev = abs(dev);
+  div = abs(div);
+  long long q = 0, temp = 0;
+  for (int i = 31; i >= 0; --i) {
+    if (temp + (divisor << i) <= dividend) {
+      temp += divisor << i;
+      quotient |= 1LL << i;
+    }
+  }
+  if(sign==-1) q=-q;
+  return q;
+}
+// Main code
+int main() {
+  int a = 13, b = 3;
+  cout << divide(a, b) << "\n";
+  a = 33, b = -8;
+  cout << divide(a, b);
+  return 0;
+}
