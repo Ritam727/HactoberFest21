a2d7710445e555414e1c374a1af607b2c3efb6c1
A. Santhosh Kumar <58947968+Santhoshstark06@users.noreply.github.com>
2021-10-05 13:55:04 +0530

    Update palindrome.cpp

--- C++/Recursion/palindrome.cpp
+++ C++/Recursion/palindrome.cpp
@@ -1,25 +1,20 @@
-#include<iostream>
-using namespace std;
-bool p(string n,int s,int e)
-{
-    if(s==e)
-        return true;
-    if(s>e)
-        return true;
-    if(n[s]!=n[e])
-        return false;
-    
-    return p(n,s+1,e-1);
-    
-}
-
-int main(int argc,char* argv[])
-{
-    string n;
-    cin>>n;
-    if(p(n,0,n.size()-1))
-    {
-        cout<<"yes";
-    }
-    
-}
+#include <iostream>
+using namespace std;  
+int main()  
+{  
+  int n,r,sum=0,temp;    
+  cout<<"Enter the Number=";    
+  cin>>n;    
+ temp=n;    
+ while(n>0)    
+{    
+ r=n%10;    
+ sum=(sum*10)+r;    
+ n=n/10;    
+}    
+if(temp==sum)    
+cout<<"Number is Palindrome.";    
+else    
+cout<<"Number is not Palindrome.";   
+  return 0;  
+}  
